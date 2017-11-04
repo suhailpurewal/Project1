81e5e9597b80486b4abd3e63f09da3d0b834262c
byork2005 <brandonjyork@gmail.com>
2017-11-04 10:24:34 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,11 @@ var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&
 // response.docs[x].byline.original - author
 // response.docs[x].pub_date - posting date
 // response.docs[x].new_desk - 
-        $(document).ready(function(){
+$(document).ready(function(){
     $(".nav-tabs a").click(function(){
         $(this).tab('show');
        });
     });
-
-
-
 
       // $.ajax({
       //   url: queryURL,
@@ -27,14 +24,58 @@ var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&
       // 	var stockResults = Stock.response.docs;
       // 	console.log(stockResults);
 
-      	// for (var i = 0; i < stockResults.length; i++) {
-      	// 	var articleDiv = $("<div>");
-      	// 	var headline = stockResults[i].headline.main;
-      	// 	console.log("for loop running")
-      	// 	articleDiv.append("<p>" + headline);
-      	// 	articleDiv.text("<p>" + headline);
+      // 	for (var i = 0; i < stockResults.length; i++) {
+      // 		var articleDiv = $("<div>");
+      // 		var headline = stockResults[i].headline.main;
+      // 		console.log("for loop running")
+      // 		articleDiv.append("<p>" + headline);
+      // 		articleDiv.text("<p>" + headline);
       		
-      	// }
+      // 	}
+
+      $(".submitBtn").on("click", function()
+      {
+
+        var stockSymbol = $("#stockSymbol").val().trim();
+        var myStockHoldings = $("#myStockHoldings").val().trim();
+        var cryptoSymbol = $("#cryptoSymbol").val().trim();
+        var myCryptoHoldings = $("#myCryptoHoldings").val();
+        var watchSymbol = $("#watchSymbol").val().trim();
+
+        var newStockRow = $("<tr>");
+        var newCryptoRow = $("<tr>");
+        var newWatchRow = $("<tr>");
+
+
+        var newStockSymbol = $("<td>").text(stockSymbol);
+        var newStockHoldings = $("<td>").text(myStockHoldings);
+        var newStockValue = $("<td>").text("");
+        var newCryptoSymbol = $("<td>").text(cryptoSymbol);
+        var newCryptoHoldings = $("<td>").text(myCryptoHoldings);
+        var newCryptoValue = $("<td>").text("");
+        var newWatchSymbol = $("<td>").text(watchSymbol);
+        var newWatchValue = $("<td>").text("");
+        var newWatchDate = $("<td>").text("");
+
+        newStockRow.append(newStockSymbol);
+        newStockRow.append(newStockValue);
+        newStockRow.append(newStockHoldings);
+        newCryptoRow.append(newCryptoSymbol);
+        newCryptoRow.append(newCryptoHoldings);
+        newCryptoRow.append(newCryptoValue);
+        newWatchRow.append(newWatchSymbol);
+        newWatchRow.append(newWatchValue);
+        newWatchRow.append(newWatchDate);
+
+        $("#stocks").append(newStockRow);
+        $("#cryptos").append(newCryptoRow);
+        $("#watchList").append(newWatchRow);
+
+      });
+
+
+
+
 
 
         $(function () { 
